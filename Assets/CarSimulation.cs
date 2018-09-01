5b1796c7decc89ec4deec0bef8e2d4d7dd23e2e6
DallonF <dallon.feldner@gmail.com>
2018-08-31 23:21:06 -0500

    Trail curves around towers

--- Assets/CarSimulation.cs
+++ Assets/CarSimulation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarSimulation : MonoBehaviour
 {
 	public float CarRadius = 2;
+	public Vector3 offset = Vector3.up;
 	public LineRenderer lineRenderer;
 	public CarDrive car;
 
@@ -15,15 +16,23 @@ public class CarSimulation : MonoBehaviour
 
 	void Update()
 	{
+		var allTowers = FindObjectsOfType<Tower>();
 		var timestep = 0.016f;
 		var positions = new List<Vector3>();
 		var currentPosition = car.transform.position;
 		var forward = car.transform.forward;
-		positions.Add(currentPosition);
+		positions.Add(currentPosition + offset);
 		for (int i = 0; i < 1000; i++)
 		{
+			foreach (var tower in allTowers)
+			{
+				if (tower.IsCarAffected(currentPosition))
+				{
+					forward = Quaternion.AngleAxis(tower.TurnEffect * timestep, Vector3.up) * forward;
+				}
+			}
 			currentPosition += forward * car.Speed * timestep;
-			positions.Add(currentPosition);
+			positions.Add(currentPosition + offset);
 			if (Physics.CheckSphere(currentPosition, CarRadius, LayerMask.GetMask("Default")))
 			{
 				break;
