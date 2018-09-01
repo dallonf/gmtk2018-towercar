f53d73bae326ed5dca5cad8c4bda1a7518e53431
DallonF <dallon.feldner@gmail.com>
2018-08-31 21:45:07 -0500

    Car moves

---
+++ Assets/CarDrive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDrive : MonoBehaviour
+{
+	public float Speed;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		var newPosition = transform.position + Speed * Time.deltaTime * transform.forward;
+		transform.position = newPosition;
+	}
+}
