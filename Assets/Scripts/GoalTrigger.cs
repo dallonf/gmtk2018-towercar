58b1e45bd0fcac63ef256b55b2744e5233b5448e
DallonF <dallon.feldner@gmail.com>
2018-09-01 22:33:48 -0500

    Goal fireworks

--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -3,4 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
-{ }
+{
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.GetComponent<CarDrive>())
+    {
+      GetComponent<GoalBehavior>().TriggerFireworks();
+      GetComponent<AudioSource>().Play();
+    }
+  }
+}
